84e57d31389a1cc886653d694cb082e41477b36e
UmedaMirai <umdmira128@gmail.com>
2022-04-05 09:38:22 +0900

    test: userエンティティ関連のテストを追加

--- src/domain/user/__tests__/user.spec.ts
+++ src/domain/user/__tests__/user.spec.ts
@@ -16,4 +16,26 @@ describe('domain/user', () => {
     const userEntity = new User(userProps);
     expect(userEntity).toBeInstanceOf(User);
   });
+
+  it('user-name-voを生成', () => {
+    const name = 'umeda';
+    const userName = new UserNameVO(name);
+    expect(userName).toBeInstanceOf(UserNameVO);
+    expect(userName.value).toMatch(name);
+  });
+
+  it('user-email-voを生成', () => {
+    const email = 'u.mirai@gmail.com';
+    const UserEmail = new UserEmailVO(email);
+    expect(UserEmail).toBeInstanceOf(UserEmailVO);
+    expect(UserEmail.value).toMatch(email);
+  });
+
+  it('user-status-voを生成', () => {
+    const status = 'Active';
+    const UserStatus = new UserStatusVO(status);
+    expect(UserStatus).toBeInstanceOf(UserStatusVO);
+    expect(UserStatus.value).toMatch(status);
+    expect(UserStatus.statusId).toBe(1);
+  });
 });
