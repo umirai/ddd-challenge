b95e29012f5a605e64992d91079a999fb82ddb34
UmedaMirai <umdmira128@gmail.com>
2022-04-10 19:30:31 +0900

    feat: teamエンティティにメソッドを追加

--- src/domain/team/team.ts
+++ src/domain/team/team.ts
@@ -9,10 +9,15 @@ export type TeamProps = {
 
 export class Team {
   private _props: TeamProps
+  private MIN_MEMBERS_COUNT = 3
 
   public constructor(props: TeamProps) {
     if (this.isInvalid(props.pairs)) throw new Error('チームには3名以上のメンバーが必要です。')
     this._props = props
+  }
+
+  get pairs() {
+    return this._props.pairs
   }
 
   get userIdList() {
@@ -29,14 +34,61 @@ export class Team {
   }
 
   private isInvalid(pairs: Pair[]): boolean {
-    return !(3 <= this.getUserIdList(pairs).length)
+    return !(this.MIN_MEMBERS_COUNT <= this.getUserIdList(pairs).length)
   }
 
-  private getUserIdList(pairs) {
-    let result: string[] = []
+  private getUserIdList(pairs: Pair[]): string[] {
+    let teamUserIdList: string[] = []
     pairs.map((pair) => {
-      pair.userIdList.map((userId) => result.push(userId))
+      pair.userIdList.map((userId) => teamUserIdList.push(userId))
     })
-    return result
+    return teamUserIdList
+  }
+
+  public getMinimumPair(): Pair {
+    return this.pairs.reduce((minPair: Pair, currentPair: Pair): Pair => {
+      if (minPair === null) minPair = currentPair
+      if (minPair.userIdList.length > currentPair.userIdList.length) minPair = currentPair
+      return minPair
+    }, null)
+  }
+
+  public generateNonDuplicatePairName(): string {
+    let nonDuplicatePairName = ''
+    const alphabets = 'abcdefghijklmnopqrstuvwxyz'.split('')
+    const pairNames = this.pairs.map((pair) => pair.pairName)
+    for (let i = 0; i < alphabets.length; i++) {
+      if (!pairNames.includes(alphabets[i])) {
+        nonDuplicatePairName = alphabets[i]
+        break
+      }
+    }
+    return nonDuplicatePairName
+  }
+
+  public getPairByUserId(userId: string): Pair {
+    return this.pairs.filter((pair) => pair.userIdList.includes(userId))[0]
+  }
+
+  public addPair(pair: Pair): void {
+    this.pairs.push(pair)
+  }
+
+  public removeUser(userId: string) {
+    const pair = this.getPairByUserId(userId)
+    const index = this.userIdList.indexOf(userId)
+    pair.userIdList.splice(index, 1)
+
+    // テスト用
+    const alert = []
+    if (this.userIdList.length < this.MIN_MEMBERS_COUNT) {
+      alert.push('team')
+      // console.log('チーム人数が2名以下になっている旨を管理者通知')
+    }
+    if (pair.userIdList.length < pair.minMembersCount) {
+      alert.push('pair')
+      // console.log('ペア人数が1名以下になっている旨を管理者通知')
+    }
+    return alert
   }
 }
