d3fe4be8516564f90178e328c714c200790c8dce
UmedaMirai <umdmira128@gmail.com>
2022-04-13 13:02:37 +0900

    test: user-service.tsのテストを追加

---
+++ src/domain/__tests__/services/user-service.spec.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client"
+import { UserRepo } from "src/infra/user/user-repo"
+import { UserService } from "src/domain/services/user-service"
+import { UserEmailVO } from "src/domain/user/user-email-vo"
+
+describe('user-service.ts', () => {
+  const prisma = new PrismaClient()
+  const userRepo = new UserRepo(prisma)
+  const userService = new UserService(userRepo)
+
+  it('重複しないメールアドレスを受け取ってfalseを返す', async () => {
+    const userEmailVO = new UserEmailVO('no-dupulicated@email.com')
+    expect(await userService.duplicatedEmail(userEmailVO)).toBeFalsy()
+  })
+
+  it('重複したメールアドレスを受け取ってtrueを返す', async () => {
+    const userEmailVO = new UserEmailVO('u.mirai@gmail.com')
+    expect(await userService.duplicatedEmail(userEmailVO)).toBeTruthy()
+  })
+})
