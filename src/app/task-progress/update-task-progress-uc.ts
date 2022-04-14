e6be75f1bab973e8973471ba314e497f3a1fc4d6
UmedaMirai <umdmira128@gmail.com>
2022-04-14 22:10:52 +0900

    feat: 課題進捗を更新するユースケースを追加

---
+++ src/app/task-progress/update-task-progress-uc.ts
@@ -0,0 +1,34 @@
+import { TaskProgressVO } from "src/domain/task-progress/task-progress-vo"
+import { ITaskProgressRepo } from "src/domain/task-progress/task-progress-repo-interface"
+import { TaskStatusProps, TaskStatusVO } from "src/domain/task-progress/task-status-vo"
+
+export type UpdateTaskProgressParams = {
+  userId: string,
+  taskId: string,
+  status: TaskStatusProps
+}
+
+export class UpdateTaskProgressUC {
+  private readonly taskProgressRepo: ITaskProgressRepo
+
+  public constructor(taskProgressRepo: ITaskProgressRepo) {
+    this.taskProgressRepo = taskProgressRepo
+  }
+
+  public async do(params: UpdateTaskProgressParams): Promise<TaskProgressVO> {
+    const { userId, taskId, status } = params
+
+    const taskProgress = new TaskProgressVO({
+      userId: userId,
+      taskId: taskId,
+      status: new TaskStatusVO(status)
+    })
+
+    const currentTaskProgress = await this.taskProgressRepo.findByUserIdAndTaskId(userId, taskId)
+    if (currentTaskProgress.isDone()) {
+      throw new Error('一度完了した課題ステータスは変更できません。')
+    }
+
+    return await this.taskProgressRepo.update(taskProgress)
+  }
+}
