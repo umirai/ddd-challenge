1ab72aabd09ef59d2c543297701d13df9b2da155
UmedaMirai <umdmira128@gmail.com>
2022-04-14 20:12:44 +0900

    feat: チーム一覧を取得するユースケースを追加

---
+++ src/app/team/find-all-teams-uc.ts
@@ -0,0 +1,33 @@
+import { Pair } from "src/domain/team/pair"
+import { PairDTO } from "src/domain/team/pair-dto"
+import { TeamDTO } from "src/domain/team/team-dto"
+import { ITeamRepo } from "src/domain/team/team-repo-interface"
+
+export class FindAllTeamsUC {
+  private readonly teamRepo: ITeamRepo
+
+  public constructor(teamRepo: ITeamRepo) {
+    this.teamRepo = teamRepo
+  }
+
+  public async do(): Promise<TeamDTO[]> {
+    try {
+      const teams = await this.teamRepo.findAll()
+      return teams.map((team) => {
+        return new TeamDTO(
+          team.id,
+          team.teamName,
+          team.pairs.map((pair) => {
+            return new PairDTO(
+              pair.id,
+              pair.pairName,
+              pair.userIdList,
+            )
+          })
+        )
+      })
+    } catch {
+      throw new Error('チームの取得に失敗しました。')
+    }
+  }
+}
