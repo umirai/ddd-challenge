aa696a4d7063202f356bc12938db831a4b828b07
UmedaMirai <umdmira128@gmail.com>
2022-04-20 07:22:36 +0900

    test: task-progress-paginationに関連するユースケースのテストを追加

---
+++ src/app/__tests__/task-progress-pagination-uc.spec.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client"
+import { TaskProgressPaginationQS } from "src/infra/query-service/task-progress-pagination-qs"
+import { FindTaskProgressPaginationUC } from "src/app/task-progress-pagenation/find-task-progress-pagination-uc"
+import { TaskProgressPaginationDTO } from "src/app/query-service/task-progress-pagenation-qs-interface"
+
+describe('app/task-progress-pagination', () => {
+  it('特定課題、特定ステータスの一覧をページングで取得する', async () => {
+    const params = {
+      taskIdList: ['1', '2'],
+      taskStatusId: 2,
+      pageNumber: 1,
+    }
+    const prisma = new PrismaClient({ log: ["query"] })
+    const taskProgressPaginatinQS = new TaskProgressPaginationQS(prisma)
+    const usecase = new FindTaskProgressPaginationUC(taskProgressPaginatinQS)
+    const result = await usecase.do(params)
+    expect(result[0]).toBeInstanceOf(TaskProgressPaginationDTO)
+  })
+})
