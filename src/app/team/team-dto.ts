c6acdd49cb96c3a61268c2bbd9e19f360f86617c
UmedaMirai <umdmira128@gmail.com>
2022-05-14 10:01:48 +0900

    refactor: app/team配下のクラスをパラメータープロパティ宣言で初期化するようリファクタ

--- src/app/team/team-dto.ts
+++ src/app/team/team-dto.ts
@@ -1,17 +1,9 @@
 import { PairDTO } from "src/app/team/pair-dto"
 
 export class TeamDTO {
-  public id: string
-  public teamName: number
-  public pairs: PairDTO[]
-
-  public constructor(
-    id: string,
-    teamName: number,
-    pairs: PairDTO[],
-  ) {
-    this.id = id
-    this.teamName = teamName
-    this.pairs = pairs
-  }
+  constructor(
+    public id: string,
+    public teamName: number,
+    public pairs: PairDTO[],
+  ) {}
 }
